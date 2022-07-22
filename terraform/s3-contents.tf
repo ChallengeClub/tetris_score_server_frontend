0e3fb7e2a717ee18fdac8f08b75c96107b0b08f8
EndoNrak <spheniscushumboldtievangelion@gmail.com>
2022-07-23 01:01:07 +0900

    fix s3.tf

--- terraform/s3-contents.tf
+++ terraform/s3-contents.tf
@@ -1,7 +1,8 @@
 resource "aws_s3_bucket_object" "index_page" {
   bucket       = aws_s3_bucket.bucket.id
+  for_each     = fileset(path.module, "../app/build/web/**/*")
   key          = "index.html"
-  source       = "../app/build/web/index.html"
+  source       = "${path.module}/${each.value}"
   content_type = "text/html"
   etag         = filemd5("../app/build/web/index.html")
 }
