824c89e8a3de4b1057ddd7b069380cea66a72a65
EndoNrak <endo.noriaki.64m@kyoto-u.jp>
2022-11-08 23:11:23 +0900

    add comment

--- app/lib/view_model/form_state_notifier.dart
+++ app/lib/view_model/form_state_notifier.dart
@@ -26,27 +26,32 @@ class FormError extends FormState {
 class FormStateNotifier extends StateNotifier<FormState> {
   final FormRepository _formRepository;
   FormStateNotifier(this._formRepository): super(FormInitial());
+
   Future<void> submitMessage(FormModel data) async{
+    // check internet connection
     ConnectivityResult connectivityResult = await (Connectivity().checkConnectivity());
     if (connectivityResult == ConnectivityResult.none) {
       state = FormError("error internet disconnected");
-    }else{
-      try{
-      bool res;
-      state = FormSubmitting();
-      res = await _formRepository.checkExistBranch(data);
-      if (res==false){
-        state = FormError("no such repository or branch\ngithub.com/${data.user_name}/${data.repository_name}/${data.branch_name}");
-        return;
-      }
-      res = await _formRepository.sendRequestToAPI(data);
-      state = res ? FormSubmitted() : FormError("failed to submit form to api");
-      } catch(e){
-        state = FormError("error occured\n${e}");
-      }
+      return;
+    }
+    try{
+    bool res;
+    state = FormSubmitting();
+
+    res = await _formRepository.checkExistBranch(data);
+    if (res==false){
+      state = FormError("no such repository or branch\ngithub.com/${data.user_name}/${data.repository_name}/${data.branch_name}");
+      return;
+    }
+
+    res = await _formRepository.sendRequestToAPI(data);
+    state = res ? FormSubmitted() : FormError("failed to submit form to api");
+    } catch(e){
+      state = FormError("error occured\n${e}");
     }
     
   }
+
   void initializeState(){
     state = FormInitial();
   }
