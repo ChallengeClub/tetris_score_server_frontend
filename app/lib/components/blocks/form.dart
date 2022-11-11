f85c6683f369b91d2f4bb8122ffccd2559b50f55
EndoNrak <endo.noriaki.64m@kyoto-u.jp>
2022-11-12 00:49:40 +0900

    create option expantion panel list

--- app/lib/components/blocks/form.dart
+++ app/lib/components/blocks/form.dart
@@ -18,6 +18,9 @@ class SubmitForm extends HookConsumerWidget {
     final _repositoryNameFormController = useTextEditingController(text: "tetris");
     final _branchFormController = useTextEditingController(text: "master");
     final _levelFormController = useTextEditingController(text: "1");
+    final _predictWeightPathController = useTextEditingController(text: "outputs/latest/best_weight.pt");
+    final _trialNumberController = useTextEditingController(text: "1");
+    final _gameModeController = useTextEditingController(text: "default");
     final _formCardHeight = _screenSize.height * 0.11;
     final _formCardWidth = _screenSize.width * 0.5;
     final _state = ref.watch(formStateNotifierProvider);
@@ -26,105 +29,168 @@ class SubmitForm extends HookConsumerWidget {
       key: _formKey,
       child: Container(
         width: _screenSize.width * 0.7,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Card(
-              color: Theme.of(context).colorScheme.surfaceVariant,
-              child: Center(
-                child: SizedBox(
-                  width: _formCardWidth,
-                  height: _formCardHeight,
-                  child: TextFormField(
-                    autofocus: true,
-                    decoration: const InputDecoration(
-                      labelText: 'user name',
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Card(
+                color: Theme.of(context).colorScheme.surfaceVariant,
+                child: Center(
+                  child: SizedBox(
+                    width: _formCardWidth,
+                    height: _formCardHeight,
+                    child: TextFormField(
+                      autofocus: true,
+                      decoration: const InputDecoration(
+                        labelText: 'user name',
+                      ),
+                      controller: _userNameFormController,
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
                     ),
-                    controller: _userNameFormController,
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter some text';
-                      }
-                      return null;
-                    },
                   ),
                 ),
               ),
-            ),Card(
-              color: Theme.of(context).colorScheme.surfaceVariant,
-              child: Center(
-                child: SizedBox(
-                  width: _formCardWidth,
-                  height: _formCardHeight,
-                  child: TextFormField(
-                    autofocus: true,
-                    decoration: const InputDecoration(
-                      labelText: 'repository name',
+              Card(
+                color: Theme.of(context).colorScheme.surfaceVariant,
+                child: Center(
+                  child: SizedBox(
+                    width: _formCardWidth,
+                    height: _formCardHeight,
+                    child: TextFormField(
+                      autofocus: true,
+                      decoration: const InputDecoration(
+                        labelText: 'repository name',
+                      ),
+                      controller: _repositoryNameFormController,
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
                     ),
-                    controller: _repositoryNameFormController,
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter some text';
-                      }
-                      return null;
-                    },
                   ),
                 ),
               ),
-            ),
-            Card(
-              color: Theme.of(context).colorScheme.surfaceVariant,
-              child: Center(
-                child: SizedBox(
-                  width: _formCardWidth,
-                  height: _formCardHeight,
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'branch name',
+              Card(
+                color: Theme.of(context).colorScheme.surfaceVariant,
+                child: Center(
+                  child: SizedBox(
+                    width: _formCardWidth,
+                    height: _formCardHeight,
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'branch name',
+                      ),
+                      controller: _branchFormController,
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
                     ),
-                    controller: _branchFormController,
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter some text';
-                      }
-                      return null;
-                    },
                   ),
                 ),
               ),
-            ),
-            Card(
-              color: Theme.of(context).colorScheme.surfaceVariant,
-              child: Center(
-                child: SizedBox(
-                  width: _formCardWidth,
-                  height: _formCardHeight,
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'game level',
+              Card(
+                color: Theme.of(context).colorScheme.surfaceVariant,
+                child: Center(
+                  child: SizedBox(
+                    width: _formCardWidth,
+                    height: _formCardHeight,
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'game level',
+                      ),
+                      controller: _levelFormController,
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        List levels = ["1", "2", "3"];
+                        if (!levels.contains(value)) {
+                          return 'Value must be in ${levels}';
+                        }
+                        return null;
+                      },
                     ),
-                    controller: _levelFormController,
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter some text';
-                      }
-                      List levels = ["1", "2", "3"];
-                      if (!levels.contains(value)) {
-                        return 'Value must be in ${levels}';
-                      }
-                      return null;
-                    },
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: ((){
-                if (_state is form.FormInitial){
-                  return ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) { // validate form data when button pressed
+              Card(
+                child: ExpansionTile(
+                title: Text('options'),
+                controlAffinity: ListTileControlAffinity.leading,
+                children: <Widget>[
+                  Center(
+                    child: SizedBox(
+                      width: _formCardWidth,
+                      height: _formCardHeight,
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'game mode ["default", "predict", "predict_sample", "predict_sample2"]',
+                        ),
+                        controller: _gameModeController,
+                        validator: (String? value) {
+                          List game_modes = ["default", "predict", "predict_sample", "predict_sample2"];
+                          if (!game_modes.contains(value)) {
+                            return 'must be in ${game_modes}';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                  ),
+                  Center(
+                    child: SizedBox(
+                      width: _formCardWidth,
+                      height: _formCardHeight,
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'predict weight path',
+                        ),
+                        controller: _predictWeightPathController,
+                      ),
+                    ),
+                  ),
+                  Center(
+                    child: SizedBox(
+                      width: _formCardWidth,
+                      height: _formCardHeight,
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'trial number',
+                        ),
+                        controller: _trialNumberController,
+                        validator: (String? value) {
+                          if (value==null || int.parse(value, onError: (value) => 0)<=0) {
+                            return 'please input positive integer';
+                          } else if (int.parse(value, onError: (value) => 0)>10){
+                            return 'trial number cannot be more than 10 ;;';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+                )
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: ((){
+                  if (_state is form.FormInitial){
+                    return ElevatedButton(
+                      onPressed: () {
+                        if (!_formKey.currentState!.validate()) {// validate form data when button pressed
+                          return;
+                        }
                         ref.read(formStateNotifierProvider.notifier).submitMessage(
                           FormModel(
                             _userNameFormController.text,
@@ -132,42 +198,42 @@ class SubmitForm extends HookConsumerWidget {
                             _branchFormController.text,
                             1000,
                             int.parse(_levelFormController.text),
-                            "default",
+                            _gameModeController.text,
                             180,
                             185,
-                            "",
-                            10
+                            _predictWeightPathController.text,
+                            int.parse(_trialNumberController.text),
                           )
                         );
-                      }
-                    },
-                    child: const Text("Submit"),
-                  );
-                } else if (_state is form.FormSubmitting){
-                  return CircularProgressIndicator();
-                } else if (_state is form.FormSubmitted){
-                  return Text("form successfully submitted");
-                } else if (_state is form.FormError){
-                  return Text(_state.message);
-                } else {
-                 return Text("unexpected error occured\n${_state}");
-                }
-              })(),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 0.0),
-              child: ((){
-                if (_state is form.FormError || _state is form.FormSubmitted){
-                  return ElevatedButton(
-                    onPressed: (){
-                      ref.read(formStateNotifierProvider.notifier).initializeState();
-                    },
-                    child: const Text("OK"),
-                  );
-                } 
-              })(),
-            )
-          ],
+                      },
+                      child: const Text("Submit"),
+                    );
+                  } else if (_state is form.FormSubmitting){
+                    return CircularProgressIndicator();
+                  } else if (_state is form.FormSubmitted){
+                    return Text("form successfully submitted");
+                  } else if (_state is form.FormError){
+                    return Text(_state.message);
+                  } else {
+                  return Text("unexpected error occured\n${_state}");
+                  }
+                })(),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 0.0),
+                child: ((){
+                  if (_state is form.FormError || _state is form.FormSubmitted){
+                    return ElevatedButton(
+                      onPressed: (){
+                        ref.read(formStateNotifierProvider.notifier).initializeState();
+                      },
+                      child: const Text("OK"),
+                    );
+                  } 
+                })(),
+              )
+            ],
+          ),
         ),
       ),
     );
