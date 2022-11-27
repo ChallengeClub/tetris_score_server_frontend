39a78e11e67d35227df46a372e99dcc4b89dffd5
EndoNrak <endo.noriaki.64m@kyoto-u.jp>
2022-11-27 12:06:32 +0900

    refactor datatable

--- app/lib/components/blocks/results_table.dart
+++ app/lib/components/blocks/results_table.dart
@@ -5,38 +5,34 @@ import 'package:provider/provider.dart';
 
 import '../../model/result_model.dart';
 import '../../view_model/providers.dart';
+import '../parts/result_dialog.dart'as ResultDialog;
 
 class ResultsTable extends HookConsumerWidget{
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final List<ResultModel> _results = ref.watch(resultStateNotifierProvider);
     var _screenSize = MediaQuery.of(context).size;
+    List<String> _columnList = this.getResultColumns(_screenSize.width);
 
     return SingleChildScrollView(
       child: FittedBox(
-        child: (_screenSize.width <= 500) ? this.mapToDataTableForMobilePhone(_results) : this.mapToDataTable(_results)
+        child: DataTable(
+          showCheckboxColumn: false,
+          columns: _columnList.map((String column) => DataColumn(label: Text(column))).toList(),
+          rows: _results.map((ResultModel result) => DataRow(
+            cells: this.mapToDataCells(_screenSize.width, result)
+          )).toList()
+        )
       ));
   }
-
-  DataTable mapToDataTableForMobilePhone(List<ResultModel> _results){
-    const List<String> _columnList = [
+  List<String> getResultColumns(var width){
+    List<String> res = [
         "created at",
         "status",
         "repository url"
-    ];
-    return DataTable(
-      columns: _columnList.map((String column) => DataColumn(label: Text(column))).toList(),
-      rows: _results.map((ResultModel result) => DataRow(
-        cells: <DataCell>[
-                DataCell(Text(DateTime.fromMillisecondsSinceEpoch(result.created_at).toString().substring(0, DateTime.fromMillisecondsSinceEpoch(result.created_at).toString().length-4))),
-                DataCell(Text(result.status)),
-                DataCell(Text(result.repository_url))
-              ]
-      )).toList()
-    );
-  }
-  DataTable mapToDataTable(List<ResultModel> _results){
-    const List<String> _columnList = [
+      ];
+    if (width >= 500){
+      res = [
         "created at",
         "status",
         "level",
@@ -46,22 +42,30 @@ class ResultsTable extends HookConsumerWidget{
         "game time",
         "trial number",
         "mean score"
-    ];
-    return DataTable(
-      columns: _columnList.map((String column) => DataColumn(label: Text(column))).toList(),
-      rows: _results.map((ResultModel result) => DataRow(
-        cells: <DataCell>[
-                DataCell(Text(DateTime.fromMillisecondsSinceEpoch(result.created_at).toString().substring(0, DateTime.fromMillisecondsSinceEpoch(result.created_at).toString().length-4))),
-                DataCell(Text(result.status)),
-                DataCell(Text(result.level.toString())),
-                DataCell(Text(result.repository_url)),
-                DataCell(Text(result.branch)),
-                DataCell(Text(result.game_mode)),
-                DataCell(Text(result.game_time.toString())),
-                DataCell(Text(result.trial_number.toString())),
-                DataCell(Text(result.mean_score.toString()))
-              ]
-      )).toList()
-    );
+      ];
+    }
+    return res;
+    }
+
+  List<DataCell> mapToDataCells(var width, ResultModel result){
+    List<DataCell> cells = [
+        DataCell(Text(DateTime.fromMillisecondsSinceEpoch(result.created_at).toString().substring(0, DateTime.fromMillisecondsSinceEpoch(result.created_at).toString().length-4))),
+        DataCell(Text(result.status)),
+        DataCell(Text(result.repository_url))
+      ];
+    if (width >= 500){
+      cells = [
+        DataCell(Text(DateTime.fromMillisecondsSinceEpoch(result.created_at).toString().substring(0, DateTime.fromMillisecondsSinceEpoch(result.created_at).toString().length-4))),
+        DataCell(Text(result.status)),
+        DataCell(Text(result.level.toString())),
+        DataCell(Text(result.repository_url)),
+        DataCell(Text(result.branch)),
+        DataCell(Text(result.game_mode)),
+        DataCell(Text(result.game_time.toString())),
+        DataCell(Text(result.trial_number.toString())),
+        DataCell(Text(result.mean_score.toString()))
+      ];
+    }
+    return cells;
   }
 }
