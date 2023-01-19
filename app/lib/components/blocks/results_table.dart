efd78c51b365a32cd3c8a844ff188d7e74080934
EndoNrak <spheniscushumboldtievangelion@gmail.com>
2023-01-19 20:47:10 +0900

    change result table to PlutoGrid

--- app/lib/components/blocks/results_table.dart
+++ app/lib/components/blocks/results_table.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:provider/provider.dart';
+import 'package:pluto_grid/pluto_grid.dart';
 
 import '../../model/result_model.dart' as ResultModel;
 import '../../view_model/providers.dart';
@@ -12,67 +13,127 @@ class ResultsTable extends HookConsumerWidget{
   Widget build(BuildContext context, WidgetRef ref) {
     List<ResultModel.ResultModel> _results = ref.watch(resultStateNotifierProvider);
     ref.read(resultStateNotifierProvider.notifier).sortResultsByCreatedAt();
-    var _screenSize = MediaQuery.of(context).size;
-    List<String> _columnList = this.getResultColumns(_screenSize.width);
+    List<PlutoColumn> _columnList = this.getResultColumns();
 
-    return SingleChildScrollView(
-      child: FittedBox(
-        fit: BoxFit.fitWidth,
-        child: DataTable(
-          showCheckboxColumn: false,
-          columns: _columnList.map((String column) => DataColumn(label: Text(column))).toList(),
-          rows: _results.map((ResultModel.ResultModel result) => DataRow(
-            onSelectChanged: (_) => ResultDialog.showResultDialog(context, result),
-            cells: this.mapToDataCells(_screenSize.width, result)
-          )).toList()
-        )
-      ));
+    return (() {
+          if (_results.length == 0) {
+          // 初期化後、通信中
+            return Center(
+              child: CircularProgressIndicator.adaptive(),
+            );
+          }
+          return PlutoGrid(
+                columns: _columnList,
+                rows: _results.map((ResultModel.ResultModel _result) => PlutoRow(
+                  cells: this.mapToDataCells(_result)
+                )).toList(),
+                rowColorCallback: (PlutoRowColorContext rowColorContext) {
+                  String status = rowColorContext.row.cells['status']?.value;
+                  Color color = Colors.white;
+                  if (status=="succeeded"){
+                    color = Color(0xFFE2F6DF);
+                  } else if (status=="error"){
+                    color = Color(0xFFFADBDF);
+                  }
+                  return color;
+                }
+          );
+        }
+        )();
   }
-  List<String> getResultColumns(var width){
-    List<String> res = [ // for mobile size
-        "name",
-        "created at",
-        "status",
-        "repository url"
-      ];
-    if (width >= 700){ // for desktop size
-      res = [
-        "name",
-        "created at",
-        "status",
-        "level",
-        "repository url",
-        "branch",
-        "game mode",
-        "game time",
-        "trial number",
-        "mean score"
-      ];
-    }
+    
+  List<PlutoColumn> getResultColumns(){
+    List<PlutoColumn> res = [
+      PlutoColumn(
+        title: 'Created at',
+        field: 'created_at',
+        type: PlutoColumnType.text(),
+        suppressedAutoSize: true,
+      ),
+      PlutoColumn(
+        title: 'Name',
+        field: 'name',
+        type: PlutoColumnType.text(),
+        suppressedAutoSize: true,
+      ),
+      PlutoColumn(
+        title: 'Mean Score',
+        field: 'mean_score',
+        type: PlutoColumnType.number(),
+        suppressedAutoSize: true,
+      ),
+      PlutoColumn(
+        title: 'URL',
+        field: 'repository_url',
+        type: PlutoColumnType.text(),
+        suppressedAutoSize: true,
+      ),
+      PlutoColumn(
+        title: 'Branch',
+        field: 'branch',
+        type: PlutoColumnType.text(),
+        suppressedAutoSize: true,
+      ),
+      PlutoColumn(
+        title: 'Trials',
+        field: 'trials',
+        type: PlutoColumnType.text(),
+        suppressedAutoSize: true,
+      ),
+      PlutoColumn(
+        title: 'Status',
+        field: 'status',
+        type: PlutoColumnType.text(),
+        suppressedAutoSize: true,
+      ),
+      PlutoColumn(
+        title: 'Level',
+        field: 'level',
+        type: PlutoColumnType.number(),
+        suppressedAutoSize: true,
+      ),
+      PlutoColumn(
+        title: 'GameMode',
+        field: 'game_mode',
+        type: PlutoColumnType.text(),
+        suppressedAutoSize: true,
+      ),
+      PlutoColumn(
+        title: 'Started at',
+        field: 'started_at',
+        type: PlutoColumnType.text(),
+        suppressedAutoSize: true,
+      ),
+      PlutoColumn(
+        title: 'Ended at',
+        field: 'ended_at',
+        type: PlutoColumnType.text(),
+        suppressedAutoSize: true,
+      ),
+      PlutoColumn(
+        title: 'ErrorMessage',
+        field: 'error_message',
+        type: PlutoColumnType.text()
+      ),
+    ];
     return res;
-    }
+  }
 
-  List<DataCell> mapToDataCells(var width, ResultModel.ResultModel result){
-    List<DataCell> cells = [
-        DataCell(Text(result.name)),
-        DataCell(Text(ResultModel.datetimeToString(result.created_at))),
-        DataCell(Text(result.status)),
-        DataCell(Text(result.repository_url))
-      ];
-    if (width >= 700){ // for desktop size
-      cells = [
-        DataCell(Text(result.name)),
-        DataCell(Text(ResultModel.datetimeToString(result.created_at))),
-        DataCell(Text(result.status)),
-        DataCell(Text(result.level.toString())),
-        DataCell(Text(result.repository_url)),
-        DataCell(Text(result.branch)),
-        DataCell(Text(result.game_mode)),
-        DataCell(Text(result.game_time.toString())),
-        DataCell(Text(result.trial_number.toString())),
-        DataCell(Text(result.mean_score.toString()))
-      ];
-    }
+  Map<String, PlutoCell> mapToDataCells(ResultModel.ResultModel result){
+    Map<String, PlutoCell> cells = {
+      'created_at': PlutoCell(value: ResultModel.datetimeToString(result.created_at)),
+      'name': PlutoCell(value: result.name),
+      'mean_score': PlutoCell(value: result.mean_score),
+      'level': PlutoCell(value: result.level),
+      'repository_url': PlutoCell(value: result.repository_url),
+      'branch': PlutoCell(value: result.branch),
+      'trials': PlutoCell(value: result.trial_number),
+      'status': PlutoCell(value: result.status),
+      'started_at': PlutoCell(value: ResultModel.datetimeToString(result.started_at)),
+      'ended_at': PlutoCell(value: ResultModel.datetimeToString(result.ended_at)),
+      'game_mode': PlutoCell(value: result.game_mode),
+      'error_message': PlutoCell(value: result.error_message),
+    };
     return cells;
   }
 }
