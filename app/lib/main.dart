0febfa931b23f6c1fd08c85269e0b8897f9dde3d
EndoNrak <endo.noriaki.64m@kyoto-u.jp>
2023-08-16 21:25:52 +0900

    add flutter web plugins for setUrlStrategy

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:amplify_authenticator/amplify_authenticator.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 import 'router.dart' as Router;
 
 Future main() async {
+  setUrlStrategy(PathUrlStrategy()); 
+  // パスベースのURLストラテジーを設定　これがないと、authのリダイレクト時にhttps://hogehoge/#/にリダイレクトされてしまって認可コードを取得できない
   runApp(
     ProviderScope(
       child: TetrisApp(),
