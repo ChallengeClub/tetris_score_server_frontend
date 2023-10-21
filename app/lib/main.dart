fcfab828ac70a88b2b65616837ea668c5dce1663
EndoNrak <endo.noriaki.64m@kyoto-u.jp>
2023-10-21 16:36:07 +0900

    test remove pathurlstrategy in main.dart

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'router.dart' as Router;
 
 Future main() async {
-  setUrlStrategy(PathUrlStrategy()); 
+  // setUrlStrategy(PathUrlStrategy()); 
   // パスベースのURLストラテジーを設定　これがないと、authのリダイレクト時にhttps://hogehoge/#/にリダイレクトされてしまって認可コードを取得できない
   runApp(
     ProviderScope(
